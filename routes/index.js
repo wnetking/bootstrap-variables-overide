482237562bd572c63ddd14ddb0dcb962707b5152
wnetking <wnetking1@gmail.com>
2018-03-07 22:26:02 +0200

    TEST: demo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var cool = require('cool-ascii-faces');
 
 /* GET home page. */
-router.get('/*', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/cool', function(req, res, next) {
+  res.send(cool());
 });
 
 module.exports = router;
