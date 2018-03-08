db7e8e3afd7e8676d75ba897392336c5cf60fb6a
wnetking <wnetking1@gmail.com>
2018-03-08 13:32:20 +0200

    TEST: demo-2

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,20 @@ var router = express.Router();
 var cool = require('cool-ascii-faces');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/cool', function(req, res, next) {
+router.get('/cool', function (req, res, next) {
   res.send(cool());
+});
+
+router.get('/time', function (req, res, next) {
+  var result = ''
+  var times = process.env.TIMES || 5
+  for (i = 0; i < times; i++)
+    result += i + ' ';
+  res.send(result);
 });
 
 module.exports = router;
